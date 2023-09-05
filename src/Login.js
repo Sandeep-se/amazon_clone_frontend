02b2c69bc03110fed9dfd67f5567cdf07c67a016
Sandeep <sandeep.se2021ecec@sece.ac.in>
2023-09-05 22:39:38 +0530

    a

--- src/Login.js
+++ src/Login.js
@@ -14,7 +14,7 @@ function Login() {
      const signIn=async(e)=>
     {
         e.preventDefault()
-        const response=await axios.post('https://amazon-clone-backend-x7dm.onrender.com/signIn',{email,password},{withCredentials:true},{"Content-Type":"application/json"})
+        const response=await axios.post('https://amazon-clone-backend-x7dm.onrender.com/signIn',{email,password},{withCredentials:true})
         try{
             if(response.data==='sigIn success')
             {
